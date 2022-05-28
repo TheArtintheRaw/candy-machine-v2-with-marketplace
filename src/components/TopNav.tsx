2017a0d61811a9fba98b8b11659ccacf73e434e1
Raw Art <theartintheraw@gmail.com>
2022-05-28 16:47:29 -0400

    topnav

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import { Link } from 'react-router-dom'
@@ -133,34 +134,34 @@ const DropdownAnchor = styled.li`
   }
 `
 
-// const WalletAmount = styled.div
+ const WalletAmount = styled.div`
   
-//  color: black;
-//   width: auto;
-//   padding: 5px 5px 5px 16px;
-//   min-width: 48px;
-//   min-height: auto;
-//   border-radius: 22px;
-//   background-color: var(--main-text-color);
-//   box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 6px 10px 0px rgb(0 0 0 / 14%),
-//     0px 1px 18px 0px rgb(0 0 0 / 12%);
-//   box-sizing: border-box;
-//   transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
-//     box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-//   font-weight: 500;
-//   line-height: 1.75;
-//   text-transform: uppercase;
-//   border: 0;
-//  margin: 0;
-//   display: inline-flex;
-//   outline: 0;
-//   position: relative;
-//   align-items: center;
-//   user-select: none;
-//   vertical-align: middle;
-//   justify-content: flex-start;
-//   gap: 10px;
-// `
+  color: black;
+   width: auto;
+   padding: 5px 5px 5px 16px;
+   min-width: 48px;
+   min-height: auto;
+   border-radius: 22px;
+   background-color: var(--main-text-color);
+   box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 6px 10px 0px rgb(0 0 0 / 14%),
+     0px 1px 18px 0px rgb(0 0 0 / 12%);
+  box-sizing: border-box;
+   transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+     box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+   font-weight: 500;
+   line-height: 1.75;
+   text-transform: uppercase;
+   border: 0;
+  margin: 0;
+   display: inline-flex;
+   outline: 0;
+   position: relative;
+   align-items: center;
+   user-select: none;
+   vertical-align: middle;
+   justify-content: flex-start;
+   gap: 10px;
+`
 
 const Wallet = styled.ul`
   flex: 0 0 auto;
