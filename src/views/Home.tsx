776fd0184b11748b615cb29a199bc68a7d542113
Raw Art <theartintheraw@gmail.com>
2022-06-06 00:08:32 -0400

    ll

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -37,7 +37,7 @@ const ConnectButton = styled(WalletMultiButton)`
 
 const NFT = styled(Paper)`
   min-width: 500px;
-  padding: 20px 20px 20px 20px;
+  padding: 0px 20px 20px 20px;
   flex: 1 1 auto;
   background-color: var(--card-background-color) !important;
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22) !important;
